8be479ef46ab256d4cf804f06b4bec5b0bb8f9d6
GratiaLe <24567@burnside.school.nz>
2026-05-04 14:15:41 +1200

    delete extra files and text files

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+import sqlite3
+
+DATABASE = "fighters.db"
+db = sqlite3.connect("DATABASE")
+cursor = db.cursor()
+sql = "SELECT * FROM fighters;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
