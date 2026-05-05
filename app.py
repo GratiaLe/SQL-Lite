ec82a30c0bd7e0c637dcd9b1e070631048e3e065
GratiaLe <24567@burnside.school.nz>
2026-05-05 12:20:48 +1200

    what would you like to do indentation

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def print_all_aircraft():
     cursor.execute(sql)
     results = cursor.fetchall()
     # loop through all the results
-    print(f"name                             speed     max_g    climb   range   payload")
+    print("name                             speed     max_g    climb   range   payload")
     for fighter in results:
         print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
     # loop finished here
@@ -19,21 +19,32 @@ def print_all_aircraft():
 
 # main code
 while True:
-    user_input = input("What would you like to do.\n1. Print all aircraft\n2. Print all aircraft sorted by speed\n7. Exit\n")
+    user_input = input(
+        """What would you like to do.\n
+        1. Print all aircraft\n
+        2. Print all aircraft sorted by speed\n
+        3. Print all aircraft sorted by max g force\n
+        4. Print all aircraft sorted by climb\n
+        5. Print all aircraft sorted by range\n
+        6. Print all aircraft sorted by payload\n
+        7. Exit\n
+        """
+            )
+
     if user_input == "1":
-            print_all_aircraft()
+        print_all_aircraft()
     elif user_input == "2":
-            pass
+        print_all_aircraft_by_speed()
     elif user_input == "3":
-            pass
+        pass
     elif user_input == "4":
-            pass
+        pass
     elif user_input == "5":
-            pass
+        pass
     elif user_input == "6":
-            pass
+        pass
     elif user_input == "7":
         print("Goodbye.")
         break
     else:
-        print("That was not an option\n")
+            print("That was not an option\n")
